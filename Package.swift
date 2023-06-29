34775c0f12b6514eee3debb03bde29bac8c36052
Shani Hajbi <shajbi@shutterfly.com>
2023-06-29 11:10:24 +0300

    exactDependencies
    Changed composable dependencies to exact to avoid breaking changes

--- Package.swift
+++ Package.swift
@@ -17,16 +17,16 @@ let package = Package(
     )
   ],
   dependencies: [
-    .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
-    .package(url: "https://github.com/google/swift-benchmark", from: "0.1.0"),
-    .package(url: "https://github.com/pointfreeco/combine-schedulers", from: "0.8.0"),
-    .package(url: "https://github.com/pointfreeco/swift-case-paths", from: "0.10.0"),
-    .package(url: "https://github.com/apple/swift-collections", from: "1.0.2"),
-    .package(url: "https://github.com/pointfreeco/swift-custom-dump", from: "0.7.0"),
-    .package(url: "https://github.com/pointfreeco/swift-dependencies", from: "0.1.2"),
-    .package(url: "https://github.com/pointfreeco/swift-identified-collections", from: "0.7.0"),
-    .package(url: "https://github.com/pointfreeco/swiftui-navigation", from: "0.6.0"),
-    .package(url: "https://github.com/pointfreeco/xctest-dynamic-overlay", from: "0.5.0"),
+    .package(url: "https://github.com/apple/swift-docc-plugin", exact: "1.0.0"),
+    .package(url: "https://github.com/google/swift-benchmark", exact: "0.1.0"),
+    .package(url: "https://github.com/pointfreeco/combine-schedulers", exact: "0.8.0"),
+    .package(url: "https://github.com/pointfreeco/swift-case-paths", exact: "0.10.0"),
+    .package(url: "https://github.com/apple/swift-collections", exact: "1.0.2"),
+    .package(url: "https://github.com/pointfreeco/swift-custom-dump", exact: "0.7.0"),
+    .package(url: "https://github.com/pointfreeco/swift-dependencies", exact: "0.1.2"),
+    .package(url: "https://github.com/pointfreeco/swift-identified-collections", exact: "0.7.0"),
+    .package(url: "https://github.com/pointfreeco/swiftui-navigation", exact: "0.6.0"),
+    .package(url: "https://github.com/pointfreeco/xctest-dynamic-overlay", exact: "0.5.0"),
   ],
   targets: [
     .target(
